adb715eb335f0b20354ad9cad2b12b3eb68828c7
Chan <47017162+1049224370@users.noreply.github.com>
2021-03-25 17:15:24 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-sudo python3 train.py --tokenizer_path ./gpt2/vocab.txt --batch_size 16 --gradient_accumulation 16 --raw --output_dir model_classfier/00/
+sudo python3 train.py --tokenizer_path ./gpt2/vocab.txt --batch_size 16 --gradient_accumulation 16 --raw --output_dir model_classfier/00/ --pretrained_model ./gpt2
